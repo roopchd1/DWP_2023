36642f6578cee76257842e7a79eafd909064fc24
roopchd1 <50752323+roopchd1@users.noreply.github.com>
2023-12-14 02:57:23 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -23,7 +23,7 @@ try {
     $mail->Host       = 'send.one.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'rupinder@greentanartisan.dk';                     //SMTP username
-    $mail->Password   = 'rupi@786';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
